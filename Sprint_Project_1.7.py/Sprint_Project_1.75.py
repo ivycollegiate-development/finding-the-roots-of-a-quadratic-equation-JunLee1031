0d4e949d7cb6546d218b13ef00d3a7537f0970bb
JunLee1031 <144109351+JunLee1031@users.noreply.github.com>
2024-10-04 05:47:32 +0000

    jun

---
+++ Sprint_Project_1.7.py/Sprint_Project_1.75.py
@@ -0,0 +1,12 @@
+import cmath
+def calculate_roots(a, b, c):
+    discriminant = b**2 - 4*a*c
+    root_1 = 
+    root_2 = 
+    return root_1, root_2
+  #  if discriminant > 0:
+   # elif discriminant == 0:
+    #else:
+     #   return None
+print(calculate_roots(1, 5, 6))
+print(calculate_roots(1, 2, 1))
